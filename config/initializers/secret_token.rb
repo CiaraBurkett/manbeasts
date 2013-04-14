2d79f0b156748743c0620cbf87fb4e167f9e0e5d
Ciara Burkett <ciaraburkett@gmail.com>
2013-04-14 17:32:57 -0400

    Deleted everything.  Starting over.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Manbeasts::Application.config.secret_token = 'a1c418f488aef945d45a6e36cece12f2117eba66d4e54c9dd3ce1faa059d295ed75133433a3f3c4c2857aabfa168593d6dfb246ecf41ff949ab2628800853feb'
